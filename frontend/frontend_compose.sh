00f9067d62c4b511a2f421cb414186f035a37128
ShakawathH <sh692@njit.edu>
2024-10-06 14:45:23 -0400

    Trying to Commit Correct Files

--- frontend/frontend_compose.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-# Node compose script
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,11 +4,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://cdn.dribbble.com/users/958543/screenshots/2868176/media/94b90e57dd3c9a3d3d346d350e3288f6.gif" className="App-logo" alt="logo" />
         <p>
-          AppliVare -- Coming Soon
+            AppliCare -- Coming Soon
         </p>
+        <a className="App-Link" href="https://www.figma.com/proto/BtVlwtFLWtipw3KQ2gqCy8/AppliCare-layout?node-id=1-2&node-type=canvas&t=Y1ELpDDGVvngeczp-0&scaling=scale-down&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=1%3A2" target="_blank">
+            Click here to view the layout of the webpage
+        </a>
+      
       </header>
     </div>
   );

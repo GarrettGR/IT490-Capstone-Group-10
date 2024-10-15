4c86b5e4769795ebeeddbc93d5f9f76eb28ea116
Garrett GR <grg@njit.edu>
2024-10-15 23:25:27 +0000

    got npm start working

--- frontend/frontend.js
+++ frontend/frontend.js
@@ -2,11 +2,11 @@
 
 const http = require('http');
 const hostname = '0.0.0.0'; // listen on all ports
-const port = 3311;
+const port = 7012;
 
 http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World');
+  res.end('Hello World, This is just temporary');
 }).listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });

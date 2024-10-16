a1541099433185d4c3d6ffae02435f07f0f99bb7
Garrett GR <grg@njit.edu>
2024-10-16 00:25:31 +0000

    working on backend

--- frontend/frontend_compose.sh
+++ frontend/frontend_compose.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-# Node compose script
-
-ln -s node_server.service /lib/systemd/system/node_server.service
-systemctl enable node_server.service
-# systemctl start node_server.service
-# systemctl restart node_server.service
-# systemctl status node_server.service
-# journalctl -u node_server.service
+systemctl link ./node_server.service
 systemctl daemon-reload
-# systemctl restart node_server.serviceps -ef | grep frontend.js
+systemctl start node_server.service
+systemctl enable node_server.service
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import ApplicareLoading from './Webpages/ApplicareLoading';
+import HomePage from './Webpages/HomePage';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      
-      <ApplicareLoading />
-      
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path = '/' element={<ApplicareLoading />}></Route>
+        {/* <Route path = '/' element={<HomePage />}></Route> */}
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;

50f7e4b8e16ac1be6eddc456e4d37185abf92556
Garrett GR <grg@njit.edu>
2024-10-16 03:59:32 +0000

    updaing backend

---
+++ backend/backend.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+import pika
+import sys
+import os
+import passlib
+
+print("hello")
--- backend/backend_compose.sh
+++ backend/backend_compose.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+export packages=(
+	pika
+	passlib
+	# uuid
+	# beautifulsoup
+	# playwright
+	# fastapi
+	# mariadb
+)
+
+if ! command -v "python3" 2>&1 >/dev/null; then
+	apt install python3 -y
+fi
+
+apt install -y python3-${packages[@]}

71971ecce077d3e5173ac61c678684718ccb1c68
Garrett GR <grg@njit.edu>
2024-10-20 05:04:39 +0000

    fixing application compose and systemd service for backend

--- backend/backend_compose.sh
+++ backend/backend_compose.sh
@@ -10,9 +10,14 @@ if ! command -v "python3" 2>&1 >/dev/null; then
   apt install python3-full -y
 fi
 
-apt install -y python3-${py_packages[@]}
+# apt install -y python3-${py_packages[@]}
+
+if [ -f /etc/systemd/system/backend.service ]; then
+  rm /etc/systemd/system/backend.service
+  systemctl daemon-reload
+fi
 
 systemctl link ./backend.service
 systemctl daemon-reload
-systemctl start backend.service
+systemctl start backend.servicie
 systemctl enable backend.service

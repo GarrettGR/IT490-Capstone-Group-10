0abfdb1e681c9825c7d302a204c1481280948bfe
Garrett GR <grg@njit.edu>
2024-10-20 05:06:48 +0000

    updaing database and frontend compose scripts

--- database/database_compose.sh
+++ database/database_compose.sh
@@ -9,7 +9,12 @@ if ! command -v "python3" 2>&1 >/dev/null; then
   apt install python3-full -y
 fi
 
-apt install -y python3-${py_packages[@]}
+# apt install -y python3-${py_packages[@]}
+
+if [ -f /etc/systemd/system/dbworker.service ]; then
+  rm /etc/systemd/system/dbworker.service
+  systemctl daemon-reload
+fi
 
 systemctl link ./dbworker.service
 systemctl daemon-reload
--- frontend/frontend_compose.sh
+++ frontend/frontend_compose.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ -f /etc/systemd/system/node_server.service ]; then
+  rm /etc/systemd/system/node_server.service
+  systemctl daemon-reload
+fi
+
 systemctl link ./node_server.service
 systemctl daemon-reload
 systemctl start node_server.service

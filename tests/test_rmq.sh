69d18057017a773ff6083bd2fecb34594c2970a9
Garrett Gonzalez-Rivas <emailforggr@gmail.com>
2024-10-22 09:37:04 -0400

    adding more tests

--- tests/test_rmq.sh
+++ tests/test_rmq.sh
@@ -5,17 +5,21 @@ RESPONSE=$(curl -s -X POST http://localhost:3000/api/form-submit -H "Content-Typ
 echo "Response: $RESPONSE"
 
 # Test 2
-RESPONSE=$(curl -s -X POST http://localhost:3000/api/form-submit -H "Content-Type: application/json" -d '{"query": "SELECT * FROM users WHERE id = 1"}')
+RESPONSE=$(curl -s -X POST http://localhost:3000/api/form-submit -H "Content-Type: application/json" -d '{"query": "SELECT * FROM users WHERE user_id = 1"}')
 echo "Response: $RESPONSE"
 
 # Test 3
-# RESPONSE=$(curl -s -X POST http://localhost:3000/api/form-submit -H "Content-Type: application/json" -d '{"greeting": "hello world"}')
-# echo "Response: $RESPONSE"
+RESPONSE=$(curl -s -X POST http://localhost:3000/api/form-submit -H "Content-Type: application/json" -d '{"greeting": "hello world"}')
+echo "Response: $RESPONSE"
 
 # Test 4
-# RESPONSE=$(curl -s -X POST http://localhost:3000/api/form-submit -H "Content-Type: application/json" -d '{"query": "INSERT INTO users (username, email, password_hash) VALUES (\"j_doe123\", \"john.doe@example.com\", \"SALT#password123#HASH\")"}')
-# echo "Response: $RESPONSE"
+RESPONSE=$(curl -s -X POST http://localhost:3000/api/form-submit -H "Content-Type: application/json" -d '{"query": "INSERT INTO users (username, email, password_hash) VALUES (\"j_doe123\", \"jane.doe@example.org\", \"SALT#password123#HASH\")"}')
+echo "Response: $RESPONSE"
 
 # Test 5
-# RESPONSE=$(curl -s -X POST http://localhost:3000/api/form-submit -H "Content-Type: application/json" -d '{"query": "SELECT * FROM users WHERE email = \"john.doe@example.com\""}')
-# echo "Response: $RESPONSE"
+RESPONSE=$(curl -s -X POST http://localhost:3000/api/form-submit -H "Content-Type: application/json" -d '{"query": "SELECT * FROM users WHERE email = \"john.doe@example.com\""}')
+echo "Response: $RESPONSE"
+
+# Test 6
+RESPONSE=$(curl -s -X POST http://localhost:3000/api/form-submit -H "Content-Type: application/json" -d '{"query": "SELECT * FROM users WHERE email = \"jane.doe@example.org\""}')
+echo "Response: $RESPONSE"

cb283f9ded8dedf851a3be149cb530281ccf9285
Garrett Gonzalez-Rivas <grg@njit.edu>
2024-11-18 16:28:11 -0500

    allow different RMQ IPs to by passed to dbworker as an Environment variable

--- database/dbworker.py
+++ database/dbworker.py
@@ -16,7 +16,7 @@ db_config = {
 
 pool = pooling.MySQLConnectionPool(pool_name="db_pool", pool_size=5, **db_config)
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('100.118.142.26', credentials=pika.PlainCredentials('admin', os.environ['rmq_passwd'])))
+connection = pika.BlockingConnection(pika.ConnectionParameters(os.environ['rmq_ip'], credentials=pika.PlainCredentials('admin', os.environ['rmq_passwd'])))
 channel = connection.channel()
 
 channel.queue_declare(queue='request_queue', durable=True, arguments={'x-message-ttl':60_000})

136b8e2b449f15011327db92fd3c4da9aee5f928
jasmrutt <jnr7@njit.edu>
2024-12-18 20:42:58 -0500

    test page

---
+++ frontend/webpages/image-test.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html data-bs-theme="light" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Applicare</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.reflowhq.com/v2/toolkit.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800&amp;display=swap">
+    <link rel="stylesheet" href="assets/css/bs-theme-overrides.css">
+    <link rel="stylesheet" href="assets/css/Login-Form-Basic-icons.css">
+</head>
+
+<body>
+    <?php include('../common/header.php'); ?>
+
+    <header class="pt-5">
+        <div class="container pt-4 pt-xl-5">
+            <div class="row pt-5">
+                <div class="col-md-8 text-center text-md-start mx-auto">
+                    <div class="text-center">
+                        <h1 class="display-4 fw-bold mb-5">Troubleshoot & Repair Your Home Appliances with Ease.</h1>
+                        <p class="fs-5 text-muted mb-5">Applicare helps you troubleshoot and repair common household appliances yourself, saving time and money. With user-friendly guides, parts recommendations, and nearby repair shop suggestions, managing your home appliances has never been easier.</p>
+                        <form class="d-flex justify-content-center flex-wrap" method="post" data-bs-theme="light">
+                            <div class="shadow-lg mb-3"><input class="form-control" type="email" name="email" placeholder="Your Email"></div>
+                            <div class="shadow-lg mb-3"><button class="btn btn-primary" type="submit">Get Started </button></div>
+                        </form>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-10 mx-auto">
+                    <div class="text-center position-relative"><img class="img-fluid" src="assets/img/illustrations/meeting.svg" style="width: 800px;"></div>
+                </div>
+            </div>
+        </div>
+    </header>
+    
+    <img src="images/default-appliance.jpg" alt="default appliances">
+   
+    <?php include('../common/footer.php'); ?>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.reflowhq.com/v2/toolkit.min.js"></script>
+    <script src="assets/js/bs-init.js"></script>
+    <script src="assets/js/startup-modern.js"></script>
+</body>
+
+</html>
